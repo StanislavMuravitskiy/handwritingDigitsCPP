8f148e18597ce27974572fc924d75c3d638b5db7
StanislavMuravitskiy <115672391+StanislavMuravitskiy@users.noreply.github.com>
2022-10-13 11:40:40 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,23 +1,23 @@
-#include <iostream>
+п»ї#include <iostream>
 #include <fstream>
 #include <vector>
 #include <ctime>
 using namespace std;
 
 
-// Задаем количество узлов в 3 слоях нейросети
+// Р—Р°РґР°РµРј РєРѕР»РёС‡РµСЃС‚РІРѕ СѓР·Р»РѕРІ РІ 3 СЃР»РѕСЏС… РЅРµР№СЂРѕСЃРµС‚Рё
 const int input_nodes = 784;
 const int hidden_nodes = 100;
 const int output_nodes = 10;
 
-//Коэффициент обучения нейросети
+//РљРѕСЌС„С„РёС†РёРµРЅС‚ РѕР±СѓС‡РµРЅРёСЏ РЅРµР№СЂРѕСЃРµС‚Рё
 const double learning_rate = 0.3;
 
 
 const double e = 2.71828;
 
 
-//Умножение матрицы на число
+//РЈРјРЅРѕР¶РµРЅРёРµ РјР°С‚СЂРёС†С‹ РЅР° С‡РёСЃР»Рѕ
 vector<vector<double>> matrixAndNumber(vector<vector<double>> a, double num) {
 	for (int i = 0; i < a.size(); i++) {
 		for (int j = 0; j < a[0].size(); j++) {
@@ -26,7 +26,7 @@ vector<vector<double>> matrixAndNumber(vector<vector<double>> a, double num) {
 	}
 	return a;
 }
-// Активационная функция - сигмоида
+// РђРєС‚РёРІР°С†РёРѕРЅРЅР°СЏ С„СѓРЅРєС†РёСЏ - СЃРёРіРјРѕРёРґР°
 vector<vector<double>> sigmoid(vector<vector<double>> a) {
 	for (int i = 0; i < a.size(); i++) {
 		for (int j = 0; j < a[0].size(); j++) {
@@ -36,7 +36,7 @@ vector<vector<double>> sigmoid(vector<vector<double>> a) {
 	return a;
 }
 
-//Вычитание из единичной матрицы
+//Р’С‹С‡РёС‚Р°РЅРёРµ РёР· РµРґРёРЅРёС‡РЅРѕР№ РјР°С‚СЂРёС†С‹
 vector<vector<double>> matrixSomething(vector<vector<double>> a) {
 	for (int i = 0; i < a.size(); i++) {
 		for (int j = 0; j < a[0].size(); j++)
@@ -45,7 +45,7 @@ vector<vector<double>> matrixSomething(vector<vector<double>> a) {
 	return a;
 }
 
-//Сложение матриц
+//РЎР»РѕР¶РµРЅРёРµ РјР°С‚СЂРёС†
 vector<vector<double>> matrixAddition(vector<vector<double>> a, vector<vector<double>> b) {
 	for (int i = 0; i < a.size(); i++) {
 		for (int j = 0; j < a[0].size(); j++) {
@@ -54,7 +54,7 @@ vector<vector<double>> matrixAddition(vector<vector<double>> a, vector<vector<do
 	}
 	return a;
 }
-//Вычитание матриц
+//Р’С‹С‡РёС‚Р°РЅРёРµ РјР°С‚СЂРёС†
 vector<vector<double>> matrixSubstration(vector<vector<double>> a, vector<vector<double>> b) {
 	for (int i = 0; i < a.size(); i++) {
 		for (int j = 0; j < a[0].size(); j++) {
@@ -64,7 +64,7 @@ vector<vector<double>> matrixSubstration(vector<vector<double>> a, vector<vector
 	return a;
 }
 
-//Умножение матриц
+//РЈРјРЅРѕР¶РµРЅРёРµ РјР°С‚СЂРёС†
 vector<vector<double>> matrixMultiplication(vector<vector<double>> a, vector<vector<double>> b) {
 	vector<vector<double>> c(a.size(), vector<double>(b[0].size(), 0));
 	for (int i = 0; i < a.size(); i++) {
@@ -79,7 +79,7 @@ vector<vector<double>> matrixMultiplication(vector<vector<double>> a, vector<vec
 	
 
 }
-//Транспонирование матрицы
+//РўСЂР°РЅСЃРїРѕРЅРёСЂРѕРІР°РЅРёРµ РјР°С‚СЂРёС†С‹
 vector<vector<double>> matrixTransposition(vector<vector<double>> a) {
 	vector<vector<double>> b(a[0].size(), vector<double>(a.size(), 0));
 	for (int i = 0; i < a.size(); i++) {
@@ -89,7 +89,7 @@ vector<vector<double>> matrixTransposition(vector<vector<double>> a) {
 	}
 	return b;
 }
-//Поэлементное умножение матриц
+//РџРѕСЌР»РµРјРµРЅС‚РЅРѕРµ СѓРјРЅРѕР¶РµРЅРёРµ РјР°С‚СЂРёС†
 vector<vector<double>> matrixMn(vector<vector<double>> a, vector<vector<double>> b) {
 	vector<vector<double>> c(a.size(), vector<double>(b[0].size(), 0));
 	for (int i = 0; i < a.size(); i++) {
@@ -118,26 +118,26 @@ int main() {
 	case 1:
 	{
 
-		//Создаем матрицы весов
+		//РЎРѕР·РґР°РµРј РјР°С‚СЂРёС†С‹ РІРµСЃРѕРІ
 		vector<vector<double>> input_to_hidden_weights(hidden_nodes, vector<double>(input_nodes, 0));
 		vector<vector<double>> hidden_to_output_weights(output_nodes, vector<double>(hidden_nodes, 0));
 
 
-		// Заполняем матрицы весов случайными значениями от 0 до 1 / корень(hidden_nodes)
+		// Р—Р°РїРѕР»РЅСЏРµРј РјР°С‚СЂРёС†С‹ РІРµСЃРѕРІ СЃР»СѓС‡Р°Р№РЅС‹РјРё Р·РЅР°С‡РµРЅРёСЏРјРё РѕС‚ 0 РґРѕ 1 / РєРѕСЂРµРЅСЊ(hidden_nodes)
 		for (int i = 0; i < hidden_nodes; i++) {
 			for (int j = 0; j < input_nodes; j++) {
 				input_to_hidden_weights[i][j] = double(rand() % 1000) / 10000.0;
 			}
 		}
 
-		// Аналогично
+		// РђРЅР°Р»РѕРіРёС‡РЅРѕ
 		for (int i = 0; i < output_nodes; i++) {
 			for (int j = 0; j < hidden_nodes; j++) {
 				hidden_to_output_weights[i][j] = double(rand() % 316) / 1000.0;
 			}
 		}
 
-		// Считываем данные обучающей выборки
+		// РЎС‡РёС‚С‹РІР°РµРј РґР°РЅРЅС‹Рµ РѕР±СѓС‡Р°СЋС‰РµР№ РІС‹Р±РѕСЂРєРё
 		ifstream training_data_input("mnist_train.txt");
 		vector<vector<double>> training_data_vector(60000, vector<double>(785, 0));
 		for (int i = 0; i < 60000; i++) {
@@ -146,23 +146,23 @@ int main() {
 			}
 		}
 
-		// Начинаем обучение с 5 эпохами
+		// РќР°С‡РёРЅР°РµРј РѕР±СѓС‡РµРЅРёРµ СЃ 5 СЌРїРѕС…Р°РјРё
 		for (int epochs = 0; epochs < 1; epochs++) {
 			for (int i = 0; i < 60000; i++) {
 				cout << "Num: " << i << endl;
 
-				// Приводим входные значения от 0.1 до 1
+				// РџСЂРёРІРѕРґРёРј РІС…РѕРґРЅС‹Рµ Р·РЅР°С‡РµРЅРёСЏ РѕС‚ 0.1 РґРѕ 1
 				for (int j = 1; j < 785; j++) {
 					training_data_vector[i][j] = training_data_vector[i][j] / 255.0 * 0.99 + 0.1;
 				}
 
-				// Создаем вектор результатов с ожидаемым значением
+				// РЎРѕР·РґР°РµРј РІРµРєС‚РѕСЂ СЂРµР·СѓР»СЊС‚Р°С‚РѕРІ СЃ РѕР¶РёРґР°РµРјС‹Рј Р·РЅР°С‡РµРЅРёРµРј
 				vector<double> results(output_nodes, 0.01);
 				int help = training_data_vector[i][0];
 				results[help] = 0.99;
 
 
-				// Приводим вектор входных  значений к матричному виду
+				// РџСЂРёРІРѕРґРёРј РІРµРєС‚РѕСЂ РІС…РѕРґРЅС‹С…  Р·РЅР°С‡РµРЅРёР№ Рє РјР°С‚СЂРёС‡РЅРѕРјСѓ РІРёРґСѓ
 				vector<vector<double>> new_inputs(1, vector<double>(784, 0));
 				int k = 0;
 				for (int j = 1; j < 785; j++) {
@@ -170,57 +170,57 @@ int main() {
 					k++;
 				}
 
-				// Приводим вектор выходных результатов к матричному виду
+				// РџСЂРёРІРѕРґРёРј РІРµРєС‚РѕСЂ РІС‹С…РѕРґРЅС‹С… СЂРµР·СѓР»СЊС‚Р°С‚РѕРІ Рє РјР°С‚СЂРёС‡РЅРѕРјСѓ РІРёРґСѓ
 				vector<vector<double>> new_results(1, vector<double>(results.size(), 0));
 				for (int j = 0; j < results.size(); j++) {
 					new_results[0][j] = results[j];
 				}
 
 
-				// Транспонируем матрицы для дальнейших вычисление
+				// РўСЂР°РЅСЃРїРѕРЅРёСЂСѓРµРј РјР°С‚СЂРёС†С‹ РґР»СЏ РґР°Р»СЊРЅРµР№С€РёС… РІС‹С‡РёСЃР»РµРЅРёРµ
 				new_results = matrixTransposition(new_results);
 				new_inputs = matrixTransposition(new_inputs);
 
 
 
-				// Находим входные значения скрытого слоя
+				// РќР°С…РѕРґРёРј РІС…РѕРґРЅС‹Рµ Р·РЅР°С‡РµРЅРёСЏ СЃРєСЂС‹С‚РѕРіРѕ СЃР»РѕСЏ
 				vector<vector<double>> hidden_inputs_value = matrixMultiplication(input_to_hidden_weights, new_inputs);
 
-				// Находим выходные значения скрытого слоя
+				// РќР°С…РѕРґРёРј РІС‹С…РѕРґРЅС‹Рµ Р·РЅР°С‡РµРЅРёСЏ СЃРєСЂС‹С‚РѕРіРѕ СЃР»РѕСЏ
 				vector<vector<double>> hidden_outputs_value = sigmoid(hidden_inputs_value);
 
 
-				//Находим входные значения выходного слоя
+				//РќР°С…РѕРґРёРј РІС…РѕРґРЅС‹Рµ Р·РЅР°С‡РµРЅРёСЏ РІС‹С…РѕРґРЅРѕРіРѕ СЃР»РѕСЏ
 				vector<vector<double>> final_inputs_value = matrixMultiplication(hidden_to_output_weights, hidden_outputs_value);
 
-				//Находим выходные значения выходного слоя
+				//РќР°С…РѕРґРёРј РІС‹С…РѕРґРЅС‹Рµ Р·РЅР°С‡РµРЅРёСЏ РІС‹С…РѕРґРЅРѕРіРѕ СЃР»РѕСЏ
 				vector<vector<double>> final_outputs_value = sigmoid(final_inputs_value);
 
-				// Находим ошибки выходного слоя
+				// РќР°С…РѕРґРёРј РѕС€РёР±РєРё РІС‹С…РѕРґРЅРѕРіРѕ СЃР»РѕСЏ
 				vector<vector<double>> output_errors = matrixSubstration(new_results, final_outputs_value);
 
 				cout << "hello";
-				// Находим ошибки скрытого слоя
+				// РќР°С…РѕРґРёРј РѕС€РёР±РєРё СЃРєСЂС‹С‚РѕРіРѕ СЃР»РѕСЏ
 				vector<vector<double>> hidden_errors = matrixMultiplication(matrixTransposition(hidden_to_output_weights), output_errors);
 
 
 
-				//Здесь дальше сложная формула для высчитывания изменения весов
+				//Р—РґРµСЃСЊ РґР°Р»СЊС€Рµ СЃР»РѕР¶РЅР°СЏ С„РѕСЂРјСѓР»Р° РґР»СЏ РІС‹СЃС‡РёС‚С‹РІР°РЅРёСЏ РёР·РјРµРЅРµРЅРёСЏ РІРµСЃРѕРІ
 
-				// Это она
+				// Р­С‚Рѕ РѕРЅР°
 				vector<vector<double>> delta_hidden_to_output_weight =
 					matrixMultiplication(matrixMn(matrixMn(output_errors, final_outputs_value), matrixSomething(final_outputs_value)),
 						matrixTransposition(hidden_outputs_value));
 
-				// Умножаем наше изменение на коэффициент обучения для дальнейшего обучения и учета предыдущих достижений
+				// РЈРјРЅРѕР¶Р°РµРј РЅР°С€Рµ РёР·РјРµРЅРµРЅРёРµ РЅР° РєРѕСЌС„С„РёС†РёРµРЅС‚ РѕР±СѓС‡РµРЅРёСЏ РґР»СЏ РґР°Р»СЊРЅРµР№С€РµРіРѕ РѕР±СѓС‡РµРЅРёСЏ Рё СѓС‡РµС‚Р° РїСЂРµРґС‹РґСѓС‰РёС… РґРѕСЃС‚РёР¶РµРЅРёР№
 				vector<vector<double>> dhtow_with_learning_rate = matrixAndNumber(delta_hidden_to_output_weight, learning_rate);
 
-				// Добавляем наши изменения к весам
+				// Р”РѕР±Р°РІР»СЏРµРј РЅР°С€Рё РёР·РјРµРЅРµРЅРёСЏ Рє РІРµСЃР°Рј
 				hidden_to_output_weights = matrixAddition(hidden_to_output_weights, dhtow_with_learning_rate);
 
 
 
-				// Здесь все анологично
+				// Р—РґРµСЃСЊ РІСЃРµ Р°РЅРѕР»РѕРіРёС‡РЅРѕ
 				vector<vector<double>> delta_input_to_hidden_weights =
 					matrixMultiplication(matrixMn(matrixMn(hidden_errors, hidden_outputs_value), matrixSomething(hidden_outputs_value)),
 						matrixTransposition(new_inputs));
@@ -229,7 +229,7 @@ int main() {
 
 				input_to_hidden_weights = matrixAddition(input_to_hidden_weights, dithw_with_learning_rate);
 
-				// Ура!!! Обучение на одном из чисел закончено, так со всеми из обучающей выборки
+				// РЈСЂР°!!! РћР±СѓС‡РµРЅРёРµ РЅР° РѕРґРЅРѕРј РёР· С‡РёСЃРµР» Р·Р°РєРѕРЅС‡РµРЅРѕ, С‚Р°Рє СЃРѕ РІСЃРµРјРё РёР· РѕР±СѓС‡Р°СЋС‰РµР№ РІС‹Р±РѕСЂРєРё
 
 
 
@@ -239,7 +239,7 @@ int main() {
 			}
 		}
 
-		// Сохраняем значения наших весов в текстовый файл
+		// РЎРѕС…СЂР°РЅСЏРµРј Р·РЅР°С‡РµРЅРёСЏ РЅР°С€РёС… РІРµСЃРѕРІ РІ С‚РµРєСЃС‚РѕРІС‹Р№ С„Р°Р№Р»
 		ofstream output("input_to_hidden_weights.txt");
 		for (int i = 0; i < hidden_nodes; i++) {
 			for (int j = 0; j < input_nodes; j++) {
@@ -247,7 +247,7 @@ int main() {
 			}
 		}
 		output.close();
-		// Здесь то же самое
+		// Р—РґРµСЃСЊ С‚Рѕ Р¶Рµ СЃР°РјРѕРµ
 		ofstream output1("hidden_to_output_weights.txt");
 		for (int i = 0; i < output_nodes; i++) {
 			for (int j = 0; j < hidden_nodes; j++) {
@@ -257,7 +257,7 @@ int main() {
 
 		output1.close();
 
-		// Считываем тестовую выборку
+		// РЎС‡РёС‚С‹РІР°РµРј С‚РµСЃС‚РѕРІСѓСЋ РІС‹Р±РѕСЂРєСѓ
 		ifstream test_data_input("mnist_test.txt");
 		vector<vector<double>> test_data_vector(10000, vector<double>(785, 0));
 		for (int i = 0; i < 10000; i++) {
@@ -267,23 +267,23 @@ int main() {
 		}
 
 
-		// Будем считать правильные ответы
+		// Р‘СѓРґРµРј СЃС‡РёС‚Р°С‚СЊ РїСЂР°РІРёР»СЊРЅС‹Рµ РѕС‚РІРµС‚С‹
 		double right_answers = 0;
 
 		for (int i = 0; i < 10000; i++) {
-			// Снова преобразуем числа
+			// РЎРЅРѕРІР° РїСЂРµРѕР±СЂР°Р·СѓРµРј С‡РёСЃР»Р°
 			for (int j = 1; j < 785; j++) {
 				test_data_vector[i][j] = test_data_vector[i][j] / 255.0 * 0.99 + 0.1;
 			}
-			// Переводим входные данные в матричный вид
+			// РџРµСЂРµРІРѕРґРёРј РІС…РѕРґРЅС‹Рµ РґР°РЅРЅС‹Рµ РІ РјР°С‚СЂРёС‡РЅС‹Р№ РІРёРґ
 			vector<vector<double>> new_inputs(1, vector<double>(784, 0));
 			for (int j = 1; j < 785; j++) {
 				new_inputs[0][j - 1] = test_data_vector[i][j];
 			}
-			// Транспонируем для дальнейших вычислений
+			// РўСЂР°РЅСЃРїРѕРЅРёСЂСѓРµРј РґР»СЏ РґР°Р»СЊРЅРµР№С€РёС… РІС‹С‡РёСЃР»РµРЅРёР№
 			new_inputs = matrixTransposition(new_inputs);
 
-			//Поэтапно считаем все входные значения
+			//РџРѕСЌС‚Р°РїРЅРѕ СЃС‡РёС‚Р°РµРј РІСЃРµ РІС…РѕРґРЅС‹Рµ Р·РЅР°С‡РµРЅРёСЏ
 			vector<vector<double>> hidden_inputs_value = matrixMultiplication(input_to_hidden_weights, new_inputs);
 			vector<vector<double>> hidden_outputs_value = sigmoid(hidden_inputs_value);
 
@@ -291,7 +291,7 @@ int main() {
 			vector<vector<double>> final_outputs_value = sigmoid(final_inputs_value);
 
 
-			// Находим цифру с наибольшей вероятностью
+			// РќР°С…РѕРґРёРј С†РёС„СЂСѓ СЃ РЅР°РёР±РѕР»СЊС€РµР№ РІРµСЂРѕСЏС‚РЅРѕСЃС‚СЊСЋ
 			double max_value = 0;
 			int best_result_number = 0;
 			for (int j = 0; j < 10; j++) {
@@ -302,7 +302,7 @@ int main() {
 				}
 			}
 
-			// Если она правильная увеличиваем счетчик
+			// Р•СЃР»Рё РѕРЅР° РїСЂР°РІРёР»СЊРЅР°СЏ СѓРІРµР»РёС‡РёРІР°РµРј СЃС‡РµС‚С‡РёРє
 			if (best_result_number == test_data_vector[i][0]) {
 				right_answers++;
 			}
@@ -311,7 +311,7 @@ int main() {
 
 		}
 
-		// Выводим точность нейросети на тестовой выборке
+		// Р’С‹РІРѕРґРёРј С‚РѕС‡РЅРѕСЃС‚СЊ РЅРµР№СЂРѕСЃРµС‚Рё РЅР° С‚РµСЃС‚РѕРІРѕР№ РІС‹Р±РѕСЂРєРµ
 		cout << "Training and tests completed" << endl;
 		cout << "Accuracy: " << right_answers / 100 << endl;
 
@@ -323,15 +323,15 @@ int main() {
 	case 2:
 	{
 
-		//Используем нейросеть для наших данных
+		//РСЃРїРѕР»СЊР·СѓРµРј РЅРµР№СЂРѕСЃРµС‚СЊ РґР»СЏ РЅР°С€РёС… РґР°РЅРЅС‹С…
 
-		//Создаем матрицы весов
+		//РЎРѕР·РґР°РµРј РјР°С‚СЂРёС†С‹ РІРµСЃРѕРІ
 		vector<vector<double>> new_input_weights(hidden_nodes, vector<double>(input_nodes, 0));
 		vector<vector<double>> new_output_weights(output_nodes, vector<double>(hidden_nodes, 0));
 
 
 
-		// Считываем все веса, которые сохранили ранее
+		// РЎС‡РёС‚С‹РІР°РµРј РІСЃРµ РІРµСЃР°, РєРѕС‚РѕСЂС‹Рµ СЃРѕС…СЂР°РЅРёР»Рё СЂР°РЅРµРµ
 		ifstream input("input_to_hidden_weights.txt");
 		for (int i = 0; i < hidden_nodes; i++) {
 			for (int j = 0; j < input_nodes; j++) {
@@ -349,7 +349,7 @@ int main() {
 		input1.close();
 
 
-		// Считываем текстовое представление нашей картинки и преобразуем
+		// РЎС‡РёС‚С‹РІР°РµРј С‚РµРєСЃС‚РѕРІРѕРµ РїСЂРµРґСЃС‚Р°РІР»РµРЅРёРµ РЅР°С€РµР№ РєР°СЂС‚РёРЅРєРё Рё РїСЂРµРѕР±СЂР°Р·СѓРµРј
 		ifstream input2("img.txt");
 		vector<double> img_vector(784);
 
@@ -360,17 +360,17 @@ int main() {
 		input2.close();
 
 
-		// Приводим к матричному виду
+		// РџСЂРёРІРѕРґРёРј Рє РјР°С‚СЂРёС‡РЅРѕРјСѓ РІРёРґСѓ
 		vector<vector<double>> new_inputs(1, vector<double>(784, 0));
 
 		for (int j = 0; j < 784; j++) {
 			new_inputs[0][j] = img_vector[j];
 		}
 
-		// Снова транспонируем
+		// РЎРЅРѕРІР° С‚СЂР°РЅСЃРїРѕРЅРёСЂСѓРµРј
 		new_inputs = matrixTransposition(new_inputs);
 
-		//Потихонечку все высчитываем
+		//РџРѕС‚РёС…РѕРЅРµС‡РєСѓ РІСЃРµ РІС‹СЃС‡РёС‚С‹РІР°РµРј
 		vector<vector<double>> hidden_inputs_value = matrixMultiplication(new_input_weights, new_inputs);
 		vector<vector<double>> hidden_outputs_value = sigmoid(hidden_inputs_value);
 
@@ -378,13 +378,13 @@ int main() {
 		vector<vector<double>> final_outputs_value = sigmoid(final_inputs_value);
 
 
-		//Выводим таблицу вероятностей значений
+		//Р’С‹РІРѕРґРёРј С‚Р°Р±Р»РёС†Сѓ РІРµСЂРѕСЏС‚РЅРѕСЃС‚РµР№ Р·РЅР°С‡РµРЅРёР№
 		for (int i = 0; i < 10; i++) {
 			cout << i << ": " << final_outputs_value[i][0] << endl;
 		}
 
 
-		//Конец. Выводы - линал и матан - сила!!!
+		//РљРѕРЅРµС†. Р’С‹РІРѕРґС‹ - Р»РёРЅР°Р» Рё РјР°С‚Р°РЅ - СЃРёР»Р°!!!
 
 
 
